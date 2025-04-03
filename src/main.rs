803f3c0295a2cb07ea9a5e0ae7a5868727fb69f8
koipix <koi.devt@gmail.com>
2025-04-03 17:28:22 +0800

    task status
    tracks task's status with pre-fixes (* = pending, - = completed)

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,28 @@ use std::env;
 use std::io::Write;
 use std::fs::{ self, OpenOptions, read_to_string };
 use dirs::home_dir;
+use owo_colors::OwoColorize;
 
-fn main() {
+pub fn main() {
     let args: Vec<String> = env::args().collect();
 
     match args[1].as_str() {
         "add" if args.len() > 2 => add_task(args[2..].join(" ")),
+        "done" if args.len() > 2 => {
+            if let Ok(index) = args[2].parse::<u8>() {
+                finish_task(index);
+            } else {
+                println!("Invalid task index: {}", args[2]);
+            }
+        },
         "list" => list_task(),
-        _ => eprint!("Usage: todo add <task> or 'todo' to show list"),
+        "help" => eprint!("Usage: 'todo add <task>' or 'todo list' to show list"),
+        "test" => println!("{}", "banana".strikethrough()),
+        _ => list_task(),
     }
 }
 
-fn add_task(task: String) {
+pub fn add_task(task: String) {
     let dir_path = home_dir().unwrap().join(".todo/");
 
     //create directory if it doesn't exist!
@@ -23,9 +33,9 @@ fn add_task(task: String) {
     }
 
     let path = &dir_path.join("todos.txt");
-    let content = read_to_string(&path).unwrap_or_else(|_| String::new());
-    let task_count = content.lines().count() + 1;
     let msg: &str = "Failed to open file";
+
+    let line = format!("* {}\n", task);
 
     let mut file = OpenOptions::new()
         .append(true)
@@ -33,19 +43,36 @@ fn add_task(task: String) {
         .open(&path)
         .expect(msg);
 
-    writeln!(file, "{} {}", task_count, task)
+    file.write_all(line.as_bytes())
         .expect(msg);
 }
 
-fn list_task() {
+pub fn finish_task(index: u8) {
     let dir_path = home_dir().unwrap().join(".todo/");
     let path = &dir_path.join("todos.txt");
 
     let content = read_to_string(&path).unwrap_or_else(|_| String::new());
 
     if !content.is_empty() {
-        for i in content.lines() {
-            println!("{}", i);
+        for (idx, task) in content.lines().enumerate() {
+            if (idx + 1) as u8 == index {
+
+            }
+        }
+    }
+}
+
+pub fn list_task() {
+    let dir_path = home_dir().unwrap().join(".todo/");
+    let path = &dir_path.join("todos.txt");
+
+    let content = read_to_string(&path).unwrap_or_else(|_| String::new());
+
+    if !content.is_empty() {
+        for (i, line) in content.lines().enumerate() {
+            let (status, task) = line.split_at(2);
+
+            println!("{} {}", i + 1, task);
         }
     } else {
         println!("No tasks to display");
